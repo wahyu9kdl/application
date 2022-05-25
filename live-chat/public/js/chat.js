24297c93b8a590d94cb467f1f95450bfec61319e
Ahmad Wahyudi <84139744+wahyu9kdl@users.noreply.github.com>
2022-05-26 06:07:23 +0700

    Add files via upload

--- live-chat/public/js/chat.js
+++ live-chat/public/js/chat.js
@@ -1,5 +1,5 @@
 // eslint-disable-next-line no-undef
-const socket = io.connect('https://wahyu9kdl.github.io/application/live-chat/');
+const socket = io.connect('http://localhost:3000');
 
 const message = document.getElementById('message-input');
 const sendMsg = document.getElementById('send-message');
